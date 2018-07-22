c0469787db8fedad69669e1717af13abcb731985
Andre Girao <andrelgirao22@hotmail.com>
2018-07-22 10:18:07 -0300

    Insercao de usuario com POST

--- src/main/java/br/com/alg/curso/mongodb/resources/UserResource.java
+++ src/main/java/br/com/alg/curso/mongodb/resources/UserResource.java
@@ -1,15 +1,18 @@
 package br.com.alg.curso.mongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.alg.curso.mongodb.domain.User;
 import br.com.alg.curso.mongodb.dto.UserDTO;
@@ -35,6 +38,15 @@ public class UserResource {
 		User obj = this.service.findById(id);
 		UserDTO objDto = new UserDTO(obj);
 		return ResponseEntity.ok(objDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = this.service.fromDTO(objDto);
+		this.service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 	
 }
--- src/main/java/br/com/alg/curso/mongodb/services/UserService.java
+++ src/main/java/br/com/alg/curso/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alg.curso.mongodb.domain.User;
+import br.com.alg.curso.mongodb.dto.UserDTO;
 import br.com.alg.curso.mongodb.exceptions.ObjectNotFoudException;
 import br.com.alg.curso.mongodb.repositories.UserRepository;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = this.repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoudException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return this.repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
 	
 }
