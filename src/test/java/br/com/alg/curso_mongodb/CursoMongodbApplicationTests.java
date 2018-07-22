e1d773aa58825373d6fc463900fbd48836663a94
Andre Girao <andrelgirao22@hotmail.com>
2018-07-22 08:37:38 -0300

    projeto criado

---
+++ src/test/java/br/com/alg/curso_mongodb/CursoMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.alg.curso_mongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
