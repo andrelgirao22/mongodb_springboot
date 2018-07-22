de8b6a32ab4d91df2ff19ff3fa12e6eaac59b078
Andre Girao <andrelgirao22@hotmail.com>
2018-07-22 12:53:25 -0300

    Acrescentando comentarios aos posts

--- src/main/java/br/com/alg/curso/mongodb/config/Instantiation.java
+++ src/main/java/br/com/alg/curso/mongodb/config/Instantiation.java
@@ -2,7 +2,6 @@ package br.com.alg.curso.mongodb.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.alg.curso.mongodb.domain.Post;
 import br.com.alg.curso.mongodb.domain.User;
 import br.com.alg.curso.mongodb.dto.AuthorDTO;
+import br.com.alg.curso.mongodb.dto.CommentDTO;
 import br.com.alg.curso.mongodb.repositories.PostRepository;
 import br.com.alg.curso.mongodb.repositories.UserRepository;
 
@@ -41,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		this.postRepository.saveAll(Arrays.asList(post1, post2));
 		
